c380a588306f2556c58bfbbbd8364637cf06de67
afeeniphillips <afeeni.phillips@bison.howard.edu>
2017-12-08 19:24:22 -0500

    Initial commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,18 +1,11 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String homePage (Model model){
-        model.addAttribute("myvar","Say hello to the people.");
-        return "hometemplate";
+    public String homePage(){
+        return "index";
     }
 }
-
-
-
-
